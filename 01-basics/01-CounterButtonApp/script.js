94055bd4a39f7e0708b40da6833d203dd313c70c
Sergey Nagornykh <serega1905@ukr.net>
2022-02-13 03:25:26 +0200

    finish vue basics

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,20 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const Counter = defineComponent({
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  template: `<button type="button" @click="count++">{{count}}</button>`,
+});
+
+// I could write less code and don't use Counter component,
+// define all options in root, but i like this approach better
+createApp({
+  // used local registration
+  components: { Counter },
+  template: `
+    <Counter/>
+  `,
+}).mount('#app');
